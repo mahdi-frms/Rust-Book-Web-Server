d4f39e869fba4d8241c4a42905090df50f3e6ee2
mahdi-frms <mahdif1380@outlook.com>
2021-07-12 17:58:30 +0430

    basic listener setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{net::TcpListener, process::exit};
 fn main() {
-    println!("Hello, world!");
+    let port : u64 = 7878;
+    match TcpListener::bind(format!("localhost:{}",port)) {
+        Ok(listener)=>{
+            println!("listening on port {}:",port);
+            for _ in listener.incoming().map(|s|s.unwrap()) {
+                println!("connected!");
+            }
+        }
+        Err(_)=>{
+            eprintln!("failed to start tcp server,exiting...");
+            exit(1);
+        }
+    } 
 }
