699b8ef46162fc37fe835c5a45c2ba0cbb2f2830
mahdi-frms <mahdif1380@outlook.com>
2021-07-12 18:32:54 +0430

    add request handler

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use std::{net::TcpListener, process::exit};
+use std::{io::Read, net::{TcpListener, TcpStream}, process::exit};
 fn main() {
     let port : u64 = 7878;
     match TcpListener::bind(format!("localhost:{}",port)) {
         Ok(listener)=>{
             println!("listening on port {}:",port);
-            for _ in listener.incoming().map(|s|s.unwrap()) {
-                println!("connected!");
+            for stream in listener.incoming().map(|s|s.unwrap()) {
+                handler(stream);
             }
         }
         Err(_)=>{
             eprintln!("failed to start tcp server,exiting...");
             exit(1);
         }
-    } 
+    }
+}
+
+fn handler(mut stream:TcpStream){
+    let mut buffer = [0u8;1024];
+    stream.read(&mut buffer).unwrap();
+    println!("client request:\n{}",String::from_utf8_lossy(&buffer));
 }
